f0787bd39c51be42871bd27a479f9dbbc044ffdd
Dang Duc Dai <dangdairare@gmail.com>
2025-12-15 19:37:55 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-console.log("NOXCODE loaded");
+const trackerCard = document.getElementById("trackerCard");
+const tracker = document.getElementById("tracker");
+const main = document.querySelector(".main");
+
+const problemInput = document.getElementById("problemName");
+const platformInput = document.getElementById("platform");
+const addBtn = document.getElementById("addPractice");
+const list = document.getElementById("practiceList");
+
+trackerCard.addEventListener("click", () => {
+    main.style.display = "none";
+    tracker.classList.remove("hidden");
+});
+
+addBtn.addEventListener("click", () => {
+    const problem = problemInput.value.trim();
+    const platform = platformInput.value.trim();
+
+    if (problem === "" || platform === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = `${problem} (${platform})`;
+
+    list.appendChild(li);
+
+    problemInput.value = "";
+    platformInput.value = "";
+});
